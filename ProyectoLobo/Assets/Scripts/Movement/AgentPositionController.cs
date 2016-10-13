214a5f6c11040054a81b3919a018954bc819755d
Rloret <al285743@uji.es>
2016-10-13 02:06:10 +0200

    Implemented Face and Align

--- ProyectoLobo/Assets/Scripts/Movement/AgentPositionController.cs
+++ ProyectoLobo/Assets/Scripts/Movement/AgentPositionController.cs
@@ -5,24 +5,25 @@ public class AgentPositionController : MonoBehaviour {
 
     #region Public Domain
     public float MaxLinearVelocity;
-    public float angularVelocity;
-
+    public float MaxAngularVelocity;
+    public float AngularVelocity;
+    public float Orientation; // stores the angle in Rads to turn like if you were looking towards the positive Z Axis counterClockWise
+    public Vector2 Position;
     public Vector2 linearVelocity;
 
     protected SteeringOutput Steering;
     #endregion
 
     #region private Domain
-    private Vector2 position;
+
  
-    private float orientation; // stores the angle in Rads to turn like if you were looking towards the positive Z Axis counterClockWise
     #endregion
 
 
 
     void Start () {
         Steering = new SteeringOutput();
-        position = new Vector2(this.transform.position.x, this.transform.position.y);
+        Position = new Vector2(this.transform.position.x, this.transform.position.y);
 	}
 
     public void SetSteering (SteeringOutput Steering)
@@ -33,28 +34,28 @@ public class AgentPositionController : MonoBehaviour {
 
 	void Update () {
 
-        position += linearVelocity * Time.deltaTime;
-        orientation += angularVelocity * Time.deltaTime;
-        if (orientation < 0.0f)
-            orientation += 360.0f;
-        else if (orientation > 360.0f)
-            orientation -= 360.0f;
+        Position += linearVelocity * Time.deltaTime;
+        Orientation += AngularVelocity * Time.deltaTime;
+        if (Orientation < 0.0f)
+            Orientation += 360.0f;
+        else if (Orientation > 360.0f)
+            Orientation -= 360.0f;
 
         transform.rotation = new Quaternion();
 
-        updateUnityVariables(ref position);
+        updateUnityVariables(ref Position);
 	}
 
     void updateUnityVariables(ref Vector2 currentPosition) {
-        this.transform.Translate(position);
-        this.transform.Rotate(Vector3.up, orientation);
-        //this.transform.position = currentPosition;
+        this.transform.Translate(Position);
+        this.transform.Rotate(Vector3.up, Orientation);
+        //this.transform.Position = currentPosition;
     }
 
     void LateUpdate()
     {
         linearVelocity += Steering.LinearAcceleration * Time.deltaTime;
-        angularVelocity += Steering.AngularAcceleration * Time.deltaTime;
+        AngularVelocity += Steering.AngularAcceleration * Time.deltaTime;
 
         if (linearVelocity.magnitude > MaxLinearVelocity)
         {
@@ -63,7 +64,7 @@ public class AgentPositionController : MonoBehaviour {
         }
 
         if (Steering.AngularAcceleration == 0.0f)
-            angularVelocity = 0.0f;
+            AngularVelocity = 0.0f;
         if (Steering.LinearAcceleration.sqrMagnitude == 0.0f)
             linearVelocity = Vector2.zero;
         Steering = new SteeringOutput();
---
+++ ProyectoLobo/Assets/Scripts/Movement/Face.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Face : Align
+{
+    protected GameObject targetAux;
+
+    public override void Awake()
+    {
+        base.Awake();
+
+        targetAux = target;
+        target = new GameObject();
+        target.AddComponent<AgentPositionController>();
+    }
+
+    void OnDestroy()
+    {
+        Destroy(target);
+    }
+
+    ///<summary>
+    ///Computes target's orientation and then delegates to Align.
+    /// A.K.A lookAt().
+    ///</summary>
+    public override SteeringOutput GetSteering()
+    {
+        
+        Vector2 direction = targetAux.GetComponent<AgentPositionController>().Position - new Vector2(transform.position.x, transform.position.y);
+
+        if (direction.magnitude > 0.0f)
+        {
+            float targetOrientation = Mathf.Atan2(direction.x,
+            direction.y);
+            targetOrientation *= Mathf.Rad2Deg;
+            target.GetComponent<AgentPositionController>().Orientation=
+            targetOrientation;
+        }
+        return base.GetSteering();
+    }
+}
