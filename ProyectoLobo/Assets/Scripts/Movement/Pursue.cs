35a4d4d6695cda5202bb1dd1cbda56dcfc140cc1
Elisabet <elilargo1994@hotmail.com>
2016-10-15 20:25:59 +0200

    Evade & pursue

---
+++ ProyectoLobo/Assets/Scripts/Movement/Pursue.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pursue : Seek {
+
+    public float maxPrediction;
+
+    private GameObject targetAux;
+    private AgentPositionController targetAgent;
+
+    public override void Awake()
+    {
+        base.Awake();
+        targetAgent = target.GetComponent<AgentPositionController>();
+        targetAux = target;
+        target = new GameObject();
+        
+    }
+
+    void Ondestroy() {
+        Destroy(targetAux);
+    }
+
+    public override SteeringOutput GetSteering()
+    {
+        Vector2 direction = targetAux.transform.position - transform.position;
+        float distance = direction.magnitude;
+        float speed = agent.linearVelocity.magnitude;
+        float prediction;
+
+        if (speed <= distance / maxPrediction) prediction = maxPrediction;
+        else prediction = distance / speed;
+
+        target.transform.position = targetAux.transform.position;
+
+        Vector3 posAux = new Vector3(targetAgent.linearVelocity.x, targetAgent.linearVelocity.y, 0f);
+        target.transform.position +=posAux* prediction;
+
+
+        return base.GetSteering();
+    }
+}
