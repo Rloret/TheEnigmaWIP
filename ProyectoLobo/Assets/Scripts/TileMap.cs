b5614d93decc92ef34cc863d188c3661ba752963
Nacho-PC\NachoPC <al285779@uji.es>
2016-10-17 00:05:05 +0200

    Primera version del nivel. TRAE UN BUG, al clickar una tile el array de los nombres da un outOfBoundaries exception.

--- ProyectoLobo/Assets/Scripts/TileMap.cs
+++ ProyectoLobo/Assets/Scripts/TileMap.cs
@@ -12,6 +12,7 @@ public class TileMap : MonoBehaviour {
     public Vector2 mapSize = new Vector2(20, 10);
     public Texture2D texture2D; // para almacenar la textura que vamos a usar para sacar las tiles
     public Vector2 tileSize = new Vector2();
+    public Vector2 tilePadding = new Vector2();
     public UnityEngine.Object[] spriteReferences;
     public Vector2 gridSize = new Vector2();
     public int pixelsToUnits = 100;
