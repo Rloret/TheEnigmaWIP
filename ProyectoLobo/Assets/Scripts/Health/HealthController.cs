363432b5e24c118342a76fcda8d128849ad687b0
Javier Lebrija <al285767@uji.es>
2016-11-15 13:30:49 +0100

    Mete Sprites Pjs, Crea Health Controller, Crea la barra de salud y hace que este sobre el PJ

---
+++ ProyectoLobo/Assets/Scripts/Health/HealthController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class HealthController : MonoBehaviour
+{
+
+    public Image Bar; //imagen de la barra de salud
+    public float MaxHealth = 100f;
+    public float CurrentHealth;
+
+    // Use this for initialization
+    void Start()
+    {
+        CurrentHealth = MaxHealth;
+
+        InvokeRepeating ("decreaseHealth", 0f, 0.5f); //va restando vida en el tiempo, para comprobar
+    }
+
+    void decreaseHealth()
+    {
+        CurrentHealth -= 2f; //He puesto un magic number aquí para probar
+        float calculateHealth = CurrentHealth / MaxHealth;
+        setHealth(calculateHealth);
+    }
+
+    void setHealth(float myHealth)
+    {
+        Bar.fillAmount = myHealth;
+    }
+}
