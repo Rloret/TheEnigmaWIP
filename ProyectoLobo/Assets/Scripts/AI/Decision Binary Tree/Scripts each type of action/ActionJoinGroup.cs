65e31eb10cea17b68161d6312475ccc83aa6fb21
Elisabet <elilargo1994@hotmsil.com>
2016-12-17 21:50:36 +0100

    Ya no hay errores cuando ofreces a una ia meterse a tu grupo ni cuando te metes al grupo de una ia

--- ProyectoLobo/Assets/Scripts/AI/Decision Binary Tree/Scripts each type of action/ActionJoinGroup.cs
+++ ProyectoLobo/Assets/Scripts/AI/Decision Binary Tree/Scripts each type of action/ActionJoinGroup.cs
@@ -22,6 +22,7 @@ public class ActionJoinGroup : Action
         GroupScript myGroup = this.GetComponent<GroupScript>();
         GroupScript leadergroup = t.GetComponent<GroupScript>();
 
+		Debug.Log("target es " + t+ " my group es: "+ myGroup+" leadergroup: "+leadergroup);
 
         myGroup.groupLeader = t ;
         myGroup.inGroup = true;
@@ -31,6 +32,8 @@ public class ActionJoinGroup : Action
         myGroup.addSingleMember(t);
         leadergroup.updateGroups(this.gameObject);
         leadergroup.makeLeader();
+
+
 
 
 		if(this.gameObject.tag!="Player") base.DestroyTrees();
